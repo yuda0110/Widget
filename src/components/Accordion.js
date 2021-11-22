86d22bd1074cf3bd43901d024a6bc3347f5d3410
Yuko Uda <yuko.uda0110@gmail.com>
2021-11-22 15:48:55 -0800

    implement accordion's functionality

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,14 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Accordion = ({ items }) => {
-  const renderedItems = items.map(item => {
+  const [activeIndex, setActiveIndex] = useState(null);
+
+  const onTitleClick = (index) => {
+    setActiveIndex(index);
+  };
+
+  const renderedItems = items.map((item, index) => {
+    const active = index === activeIndex ? 'active' : '';
     return (
       <React.Fragment key={item.title}>
-        <div className="title active">
+        <div
+          className={`title ${active}`}
+          onClick={() => onTitleClick(index)}
+        >
           <i className="dropdown icon"></i>
           {item.title}
         </div>
-        <div className="content active">
+        <div className={`content ${active}`}>
           <p>{item.content}</p>
         </div>
       </React.Fragment>
@@ -16,6 +26,7 @@ const Accordion = ({ items }) => {
   });
   return <div className="ui styled accordion">
     {renderedItems}
+    <h1>{activeIndex}</h1>
   </div>
 };
 
