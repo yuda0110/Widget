a79486b247241dace1836128ab04ba9771509bd0
Yuko Uda <yuko.uda0110@gmail.com>
2021-11-22 16:11:20 -0800

    add the search form in Search

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,7 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Search = () => {
-  return <h1>Search</h1>;
+  const [term, setTerm] = useState('');
+
+  return (
+    <div>
+      <div className="ui form">
+        <div className="field">
+          <label htmlFor="">Enter Search Term</label>
+          <input
+            value={term}
+            type="text"
+            className="input"
+            onChange={e => setTerm(e.target.value)}
+          />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Search;
